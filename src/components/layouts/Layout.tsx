ac3da71f1c3ddaa6a4ede1c9947217e27babde01
Elvis Linares <elvis.039@hotmail.com>
2025-02-18 21:58:58 -0400

    active link menu

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,7 +1,8 @@
-import { Link, Outlet, useLocation } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import style from './layout.module.css'
 import { useEffect, useState } from "react";
 import SocialRed from "../ui/SocialRed/SocialRed";
+import { NavLink } from "react-router";
 
 export default function Layout() {
 
@@ -25,13 +26,13 @@ export default function Layout() {
           <a href="./" className={`${style.logo} ${style.color_transition}`}>repoxi</a>
           <nav >
               <img onClick={handleClick} className={`${style.menu_img}`} src="/img/menu.png" alt="menu"/>
-              <div className={menuVisible?`${style.active} ${style.menu}`:`${style.deactive} ${style.menu}`}>
-                  <Link className={`${style.menu_a} ${style.color_transition}`} to="./">Inicio</Link>
+              <nav className={menuVisible?`${style.active} ${style.menu}`:`${style.deactive} ${style.menu}`}>
+                  <NavLink  className={({isActive})=>isActive?`${style.menu_a} ${style.color_transition} ${style.menuActive}`:`${style.menu_a} ${style.color_transition}`} to="./">Inicio</NavLink>
                   {/* <Link className="menu-publicidad" to="/publicity">Publicidad</Link> */}
-                  <Link className={`${style.menu_a} ${style.color_transition}`} to="/discover_neon">Descubre Nuestros Neones</Link>
-                  <Link className={`${style.menu_a} ${style.color_transition}`} to="/create_neon">Cotiza Tu Neón</Link>
-                  <Link className={`${style.menu_a} ${style.color_transition}`} to="/neon">Nuestros neones</Link>
-              </div>
+                  <NavLink  className={({isActive})=>isActive?`${style.menu_a} ${style.color_transition} ${style.menuActive}`:`${style.menu_a} ${style.color_transition}`} to="/discover_neon">Descubre Nuestros Neones</NavLink>
+                  <NavLink className={({isActive})=>isActive?`${style.menu_a} ${style.color_transition} ${style.menuActive}`:`${style.menu_a} ${style.color_transition}`} to="/create_neon">Cotiza Tu Neón</NavLink>
+                  <NavLink  className={({isActive})=>isActive?`${style.menu_a} ${style.color_transition} ${style.menuActive}`:`${style.menu_a} ${style.color_transition}`} to="/neon">Nuestros neones</NavLink>
+              </nav>
           </nav>
       </header>
 
