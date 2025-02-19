0890246366a3b69e3b8dc270eff34364a48ba2be
Elvis Linares <elvis.039@hotmail.com>
2025-02-18 22:45:49 -0400

    social red target _blank

--- src/components/ui/SocialRed/SocialRed.tsx
+++ src/components/ui/SocialRed/SocialRed.tsx
@@ -1,15 +1,14 @@
 import { faFacebook, faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import style from "./socialRed.module.css"
-import { Link } from "react-router";
 
 export default function SocialRed() {
   return (
     <>
       <div className={style.content_icon}>
-        <Link to={import.meta.env.VITE_FACEBOOK_URL}><FontAwesomeIcon icon={faFacebook} className={style.icon_awesome}/></Link>
-        <Link to={import.meta.env.VITE_INSTAGRAM_URL}><FontAwesomeIcon icon={faInstagram} className={style.icon_awesome}/></Link>
-        <Link to={import.meta.env.VITE_WHATSAPP_URL}><FontAwesomeIcon icon={faWhatsapp} className={style.icon_awesome}/></Link>
+        <a href={import.meta.env.VITE_FACEBOOK_URL} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} className={style.icon_awesome}/></a>
+        <a href={import.meta.env.VITE_INSTAGRAM_URL} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} className={style.icon_awesome}/></a>
+        <a href={import.meta.env.VITE_WHATSAPP_URL} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faWhatsapp} className={style.icon_awesome}/></a>
       </div>
     </>
     
