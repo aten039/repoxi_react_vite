e075de682f87d318cedeffa2fca5801d194fbafd
Elvis Linares <elvis.039@hotmail.com>
2025-02-19 23:52:48 -0400

    json info, neon cards

--- src/components/main/contentListNeon/ContentListNeon.tsx
+++ src/components/main/contentListNeon/ContentListNeon.tsx
@@ -4,20 +4,89 @@ import Banner from '../../ui/banner/Banner'
 import CardNeon from '../../ui/cardNeon/CardNeon'
 import style from './ContentListNeon.module.css'
 
+type Neon = {
+  title:string,
+  img:string,
+  price:string
+}
+
 export default function ContentListNeon() {
+
+
+  const neones: Neon[] = [
+    {
+      title:"Alzada fucsia",
+      img:"alzada.jpg",
+      price:""
+    },
+    {
+      title:"happy birthday blanco cálido",
+      img:"happy1.jpg",
+      price:"55"
+    },
+    {
+      title:"happy birthday blanco frio",
+      img:"happy3.jpg",
+      price:"55"
+    },
+    {
+      title:"Nails fucsia",
+      img:"nails.jpg",
+      price:"50"
+    },
+    {
+      title:"Sonrie a la vida",
+      img:"sonrie.jpg",
+      price:"50"
+    },
+    {
+      title:"si te gusta, usalo",
+      img:"si_gusta.jpg",
+      price:"45"
+    },
+    {
+      title:"Logo mdf vinil 60cm",
+      img:"abby.jpg",
+      price:"50"
+    },
+    {
+      title:"Logo mdf vinil 50cm",
+      img:"rosbely.jpg",
+      price:"45"
+    },
+    {
+      title:"Haz magia",
+      img:"magia.jpg",
+      price:"40"
+    },
+    {
+      title:"vida dulce, dulce vida",
+      img:"pinerolo.jpg",
+      price:"45"
+    },
+    {
+      title:"Street Shop",
+      img:"street2.jpg",
+      price:"45"
+    },
+  ] 
+
+  const mitad = Math.ceil(neones.length / 2);
+  const firstMitad = neones.slice(0, mitad); 
+  const secondMitad = neones.slice(mitad);
+   
   return (
     <>
         <Banner/>
         <div className={style.content_main}>
           <h1 className={style.tittle}>Explora nuestra colección de neones.</h1>
           <div className={style.content}>
-            <CardNeon title='sonrie que la vida es bella' img='sonrie.jpg'/>
-            <CardNeon title='sonrie' img='alzada.jpg'/>
-            <CardNeon title='sonrie' img='sonrie.jpg'/>
-            <CardNeon title='sonrie' img='pinerolo.jpg'/>
-            <CardNeon title='sonrie' img='si_gusta.jpg'/>
-            <CardNeon title='sonrie' img='magia.jpg'/>
-            <CardNeon title='sonrie' img='sonrie.jpg'/>
+            {
+              firstMitad.map((neon)=>{
+                return(<CardNeon title={neon.title} price={neon.price} img={neon.img} />)
+              })
+            }
+            
           </div>
           <div className={`${style.content_text } ${style.content_one}`}>
                 <h3>Cotiza tu neon personalizado</h3>
@@ -25,9 +94,11 @@ export default function ContentListNeon() {
                 <Link to={'/create_neon'} className="button">cotizar</Link>
           </div>
           <div className={style.content}>
-            <CardNeon title='sonrie' img='si_gusta.jpg'/>
-            <CardNeon title='sonrie' img='magia.jpg'/>
-            <CardNeon title='sonrie' img='sonrie.jpg'/>
+            {
+              secondMitad.map((neon)=>{
+                return(<CardNeon title={neon.title} price={neon.price} img={neon.img} />)
+              })
+            }
           </div>
           <div className={style.content_text}>
                 <h3>Descubre nuestros neones</h3>
--- src/components/ui/cardNeon/CardNeon.tsx
+++ src/components/ui/cardNeon/CardNeon.tsx
@@ -3,18 +3,18 @@ import style from './CardNeon.module.css'
 type Props = {
   title:string,
   img:string,
-
+  price:string
 }
 
-export default function CardNeon({title, img}: Props) {
+export default function CardNeon({title, img, price}: Props) {
   return (
     <>
       <div className={style.card}>
         <div className={style.card_content}>
-          <img className={style.img} src={"/img/" + img} />
+          <img className={style.img} src={"/img/neones/" + img} />
           <div className={style.content_text}>
             <h2 className={style.tittle}>{title}</h2>
-            <p>A partir de: <span>20$</span></p>
+            <p>A partir de: <span>{price}$</span></p>
           </div>
           
         </div>
